0cc776fb47e5339259675eca5548dc524aa7c550
Theodore Cipicchio <okready@users.noreply.github.com>
2021-02-18 19:28:13 -0800

    Update copyright years for 2021

--- build.rs
+++ build.rs
@@ -1,4 +1,4 @@
-// Copyright 2018-2019 Theodore Cipicchio
+// Copyright 2018-2021 Theodore Cipicchio
 //
 // Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
 // http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
